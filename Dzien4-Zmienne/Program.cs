cf5aa32005f9a0346b8a21e3945ee349df1d09f4
User@ETYCZNY <biurohetmana@gmail.com>
2024-04-03 20:48:53 +0100

    Add project files.

---
+++ Dzien4-Zmienne/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int myAge = int.MinValue;
+        uint myAge2 = uint.MinValue;
+        long myAge3 = long.MaxValue;
+
+        //Liczby zmiennoprzecinkowe
+        float myAge4 = float.MaxValue + 100;// floata uywamy do rednich naprzykad
+        decimal myAge5 = decimal.MaxValue;// Diametralnie największa
+
+    }
+}
